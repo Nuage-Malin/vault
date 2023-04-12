44aeea7d7e0466274fa6c4cd33d853f556c85d4b
Arthur <arthur.jourdan@epitech.eu>
2023-04-12 16:58:22 +1000

    chore(build.rs): add good proto file to grpc compilation through tonic

--- build.rs
+++ build.rs
@@ -1,3 +1,20 @@
-fn main() {
-	println!("hello from build.rs");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("hello compile");
+	let protobufs_dir = "third_parties/protobuf-interfaces";
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
+		.out_dir(protobufs_dir.to_string() + "/generated")
+        .compile(
+            &[protobufs_dir.to_string() + "/src/Maestro_Vault/Maestro_Vault.proto"],
+            &[protobufs_dir.to_string() + "/src"],
+
+        )
+		// .out_dir("third_parties/generated")
+        .unwrap_or_else(|e| panic!("compilation failed: {}", e));
+    Ok(())
 }
