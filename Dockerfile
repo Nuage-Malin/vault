2885be79223f99f3dd8bd705fbcd5c6088a01256
Arthur <arthur.jourdan@epitech.eu>
2023-04-22 11:03:56 +1000

    feat(docker-compose.yml Dockerfile vault.env): setup docker utils, with env and modify main to receive vault gRPC server address from env variable

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM rust:1.65
+
+RUN apt-get update
+RUN apt-get install -y protobuf-compiler
+
+WORKDIR /app
+
+COPY third_parties /app/third_parties
+COPY build.rs /app/build.rs
+COPY src /app/src
+COPY Cargo.toml /app/Cargo.toml
+COPY Cargo.toml /app/Cargo.toml
+
+RUN cargo update
+RUN cargo build
+
+CMD cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod models;
 mod maestro;
+use std::net::{SocketAddr, Ipv6Addr, SocketAddrV6};
+
 use crate::models::grpc::maestro_vault::{maestro_vault_service_server::MaestroVaultServiceServer};
 use tonic::transport::Server;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let address = "[::1]:8080".parse().unwrap();
-	let maestroService = maestro::MaestroVault::default();
+	let address_str = std::env::var("VAULT_ADDRESS").expect("VAULT_ADDRESS not set.");
+	let port_str = std::env::var("VAULT_PORT").expect("VAULT_PORT not set.");
+	let address: Ipv6Addr = address_str.parse().expect("VAULT_ADDRESS has wrong format");
+	let port: u16 = port_str.parse().expect("VAULT_PORT has wrong format");
+	let vault_address: SocketAddr = SocketAddr::V6(SocketAddrV6::new(address, port, 0, 0));
+	let my_maestro_service = maestro::MaestroVault::default();
 
-	Server::builder().add_service(MaestroVaultServiceServer::new(maestroService))
-	  .serve(address)
+	Server::builder().add_service(MaestroVaultServiceServer::new(my_maestro_service))
+	  .serve(vault_address)
 	  .await?;
 	Ok(())
 }
